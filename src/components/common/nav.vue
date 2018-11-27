fba1b9473de70ca90ba7aefe78d20c65b8e5b8e8
djtgithub <1601304143@qq.com>
2018-11-27 09:12:03 +0800

    nav.vue与page1.vue修改

--- src/components/common/nav.vue
+++ src/components/common/nav.vue
@@ -1,34 +1,38 @@
 <template>
-    <div class="bs-example" data-example-id="inverted-navbar">
-         <Menu active-name="1-2" :open-names="['1']">
-        <Submenu name="1">
-            <template slot="title">
-                <Icon type="ios-analytics" />
-                网站首页
-            </template>
-         
-                <MenuItem name="1-2" ><router-link  to="/Page1" >网站首页</router-link></MenuItem>
-           
-        </Submenu>
-        <Submenu name="2">
-            <template slot="title">
-                <Icon type="ios-filing" />
-                技术分享
-            </template>
-             <MenuItem name="1-2" ><router-link  to="/Page2" >技术分享</router-link></MenuItem>
-           <!--  <Submenu name="3">
+  <div class="bs-example" data-example-id="inverted-navbar">
+    <Menu active-name="1-2" :open-names="['1']">
+      <Submenu name="1">
+        <template slot="title">
+          <Icon type="ios-analytics" />
+          网站首页
+        </template>
+        <MenuItem name="1-2">
+        <router-link to="/Page1">网站首页</router-link>
+        </MenuItem>
+      </Submenu>
+      <Submenu name="2">
+        <template slot="title">
+          <Icon type="ios-filing" />
+          技术分享
+        </template>
+        <MenuItem name="1-2">
+        <router-link to="/Page2">技术分享</router-link>
+        </MenuItem>
+        <!--  <Submenu name="3">
                 <template slot="title">Submenu</template>
                 <MenuItem name="3-1">Option 7</MenuItem>
                 <MenuItem name="3-2">Option 8</MenuItem>
             </Submenu> -->
-        </Submenu>
-        <Submenu name="4">
-            <template slot="title">
-                <Icon type="ios-cog" />
-                个人中心
-            </template>
-            <MenuItem name="1-2" ><router-link  to="/Page2" >关于我们</router-link></MenuItem>
-            <!-- <MenuItem name="4-1">Option 9</MenuItem>
+      </Submenu>
+      <Submenu name="4">
+        <template slot="title">
+          <Icon type="ios-cog" />
+          个人中心
+        </template>
+        <MenuItem name="1-2">
+        <router-link to="/Page2">关于我们</router-link>
+        </MenuItem>
+        <!-- <MenuItem name="4-1">Option 9</MenuItem>
             <MenuItem name="4-2">Option 10</MenuItem>
             <MenuItem name="4-3">Option 11</MenuItem>
             <MenuItem name="4-4">Option 12</MenuItem>
@@ -37,48 +41,59 @@
                 <MenuItem name="3-1">Option 7</MenuItem>
                 <MenuItem name="3-2">Option 8</MenuItem>
             </Submenu> -->
-        </Submenu>
+      </Submenu>
     </Menu>
-    </div>
+  </div>
 </template>
 <script>
-    export default{
+export default {
 
 
-	}
+}
+
 </script>
 <style scoped>
-*{
-	margin:0;
-	padding: 0;
+* {
+  margin: 0;
+  padding: 0;
 }
-a:hover{
-	text-decoration: none;
+
+a:hover {
+  text-decoration: none;
 }
-.bs-example{
-	border-radius:0;
-	    position: fixed;
-    left: 0;
-    top: 52px;
-    width: 240px;
-    height: 100%;
-    background: #ccc;
+
+.bs-example {
+  border-radius: 0;
+  position: absolute;
+  left: 0;
+  top: 52px;
+  width: 240px;
+  height: 100%;
 }
-.list{
-	    width: 100%;
-    background: red;
+
+.list {
+  width: 100%;
+  background: red;
 }
-.list li{
-	background: yellow;
-	height: 40px;
-	line-height: 40px;
+
+.list li {
+  background: yellow;
+  height: 40px;
+  line-height: 40px;
 }
-.list li a{
-	text-align: left;
-	display: block;
-	width: 100%;
-	text-align: left;
-	text-indent: 10px;
-	cursor: pointer;
+
+.list li a {
+  text-align: left;
+  display: block;
+  width: 100%;
+  text-align: left;
+  text-indent: 10px;
+  cursor: pointer;
 }
+
+
+.ivu-menu-vertical.ivu-menu-light:after{
+    height: 0 !important;
+}
+
 </style>
--- src/pages/page1.vue
+++ src/pages/page1.vue
@@ -1,185 +1,279 @@
 <template>
   <div>
-    <div class="cs_list1 col">
-      <div class="bigtitle">热门排行</div>
-      <ul>
-        <li>
-          <span class="label-1">1</span>
-          <a href="">Python2爬虫学习系列教程</a>
-          <p>
-            <span class="muted">
-      			<a href="javascript:;" data-action="ding" data-id="1052" id="Addlike" class="action">
-      			<Icon type="md-heart-outline" />
-      			<span class="count">30620</span> 喜欢
-          </a>
-          </span>
-          </p>
-        </li>
-        <li>
-          <span  class="label-2">2</span>
-          <a href="">Python2爬虫学习系列教程</a>
-          <p>
-            <span class="muted">
-      			<a href="javascript:;" data-action="ding" data-id="1052" id="Addlike" class="action">
-      			<Icon type="md-heart-outline" />
-      			<span class="count">30620</span> 喜欢
-          </a>
-          </span>
-          </p>
-        </li>
-        <li>
-          <span  class="label-3">3</span>
-          <a href="">Python2爬虫学习系列教程</a>
-          <p>
-            <span class="muted">
-      			<a href="javascript:;" data-action="ding" data-id="1052" id="Addlike" class="action">
-      			<Icon type="md-heart-outline" />
-      			<span class="count">30620</span> 喜欢
-          </a>
-          </span>
-          </p>
-        </li>
-        <li>
-          <span  class="label-4">4</span>
-          <a href="">Python2爬虫学习系列教程</a>
-          <p>
-            <span class="muted">
-      			<a href="javascript:;" data-action="ding" data-id="1052" id="Addlike" class="action">
-      			<Icon type="md-heart-outline" />
-      			<span class="count">30620</span> 喜欢
-          </a>
-          </span>
-          </p>
-        </li>
-        <li>
-          <span  class="label-5">5</span>
-          <a href="">Python2爬虫学习系列教程</a>
-          <p>
-            <span class="muted">
-      			<a href="javascript:;" data-action="ding" data-id="1052" id="Addlike" class="action">
-      			<Icon type="md-heart-outline" />
-      			<span class="count">30620</span> 喜欢
-          </a>
-          </span>
-          </p>
-        </li>
-      </ul>
-      <!-- {{this.$store.state}} -->
+    <div class="tab1">
+      <div class="wrapl">
+        <div class="cs_list1 col">
+          <div class="bigtitle">热门排行</div>
+          <ul>
+            <li>
+              <span class="label-1">1</span>
+              <a href="">Python2爬虫学习系列教程</a>
+              <p>
+                <span class="muted">
+              <a href="javascript:;" data-action="ding" data-id="1052" id="Addlike" class="action">
+              <Icon type="md-heart-outline" />
+              <span class="count">30620</span> 喜欢
+              </a>
+              </span>
+              </p>
+            </li>
+            <li>
+              <span  class="label-2">2</span>
+              <a href="">Python2爬虫学习系列教程</a>
+              <p>
+                <span class="muted">
+              <a href="javascript:;" data-action="ding" data-id="1052" id="Addlike" class="action">
+              <Icon type="md-heart-outline" />
+              <span class="count">30620</span> 喜欢
+              </a>
+              </span>
+              </p>
+            </li>
+            <li>
+              <span  class="label-3">3</span>
+              <a href="">Python2爬虫学习系列教程</a>
+              <p>
+                <span class="muted">
+              <a href="javascript:;" data-action="ding" data-id="1052" id="Addlike" class="action">
+              <Icon type="md-heart-outline" />
+              <span class="count">30620</span> 喜欢
+              </a>
+              </span>
+              </p>
+            </li>
+            <li>
+              <span  class="label-4">4</span>
+              <a href="">Python2爬虫学习系列教程</a>
+              <p>
+                <span class="muted">
+              <a href="javascript:;" data-action="ding" data-id="1052" id="Addlike" class="action">
+              <Icon type="md-heart-outline" />
+              <span class="count">30620</span> 喜欢
+              </a>
+              </span>
+              </p>
+            </li>
+            <li>
+              <span  class="label-5">5</span>
+              <a href="">Python2爬虫学习系列教程</a>
+              <p>
+                <span class="muted">
+              <a href="javascript:;" data-action="ding" data-id="1052" id="Addlike" class="action">
+              <Icon type="md-heart-outline" />
+              <span class="count">30620</span> 喜欢
+              </a>
+              </span>
+              </p>
+            </li>
+          </ul>
+          <!-- {{this.$store.state}} -->
+        </div>
+        <div class="cs_list1 col">
+          <div class="bigtitle">每日推荐</div>
+          <ul>
+            <li>
+              <span class="label-1">1</span>
+              <a href="">Python2爬虫学习系列教程</a>
+              <p>
+                <span class="muted">
+              <a href="javascript:;" data-action="ding" data-id="1052" id="Addlike" class="action">
+              <Icon type="md-heart-outline" />
+              <span class="count">30620</span> 喜欢
+              </a>
+              </span>
+              </p>
+            </li>
+            <li>
+              <span  class="label-2">2</span>
+              <a href="">Python2爬虫学习系列教程</a>
+              <p>
+                <span class="muted">
+              <a href="javascript:;" data-action="ding" data-id="1052" id="Addlike" class="action">
+              <Icon type="md-heart-outline" />
+              <span class="count">30620</span> 喜欢
+              </a>
+              </span>
+              </p>
+            </li>
+            <li>
+              <span  class="label-3">3</span>
+              <a href="">Python2爬虫学习系列教程</a>
+              <p>
+                <span class="muted">
+              <a href="javascript:;" data-action="ding" data-id="1052" id="Addlike" class="action">
+              <Icon type="md-heart-outline" />
+              <span class="count">30620</span> 喜欢
+              </a>
+              </span>
+              </p>
+            </li>
+            <li>
+              <span  class="label-4">4</span>
+              <a href="">Python2爬虫学习系列教程</a>
+              <p>
+                <span class="muted">
+              <a href="javascript:;" data-action="ding" data-id="1052" id="Addlike" class="action">
+              <Icon type="md-heart-outline" />
+              <span class="count">30620</span> 喜欢
+              </a>
+              </span>
+              </p>
+            </li>
+            <li>
+              <span  class="label-5">5</span>
+              <a href="">Python2爬虫学习系列教程</a>
+              <p>
+                <span class="muted">
+              <a href="javascript:;" data-action="ding" data-id="1052" id="Addlike" class="action">
+              <Icon type="md-heart-outline" />
+              <span class="count">30620</span> 喜欢
+              </a>
+              </span>
+              </p>
+            </li>
+          </ul>
+        </div>
+        <div class="clear"></div>
+        <div class="tab2">
+           <div class="title">爬虫代理哪家强？十大付费代理详细对比评测出炉！</div>
+           <div class="content">
+             前言 随着大数据时代的到来，爬虫已经成了获取数据的必不可少的方式，做过爬虫的想必都深有体会，爬取的时候莫名其妙 IP 就被网站封掉了，毕竟各大网站也不想自己的数据被轻易地爬走。 对于爬虫来说，为了解决封禁 IP 的问题，一个有效的方式就是使用代理，使用代理之后可以让爬虫伪装自己的...
+           </div>
+        </div>
+         <div class="tab2">
+           <div class="title">破解网站登录加密–RSA</div>
+           <div class="content">
+            破解网站登录加密–RSA大家好，我是四毛，下面的是我的公众号，欢迎关注。   今天的内容主要讲的是破解一个网站的rsa加密，当然肯定不是破解这个算法，而是找到加密的参数，正确模拟这个算法即可。 1. 什么是rsa算法 下面的资料摘抄自阮一峰老师的文章， 点这里了解更多 1976年，两位美国...
+           </div>
+        </div>
+         <div class="tab2">
+           <div class="title">如何给Azure云服务器扩展云磁盘</div>
+           <div class="content">
+             前言 随着大数据时代的到来，爬虫已经成了获取数据的必不可少的方式，做过爬虫的想必都深有体会，爬取的时候莫名其妙 IP 就被网站封掉了，毕竟各大网站也不想自己的数据被轻易地爬走。 对于爬虫来说，为了解决封禁 IP 的问题，一个有效的方式就是使用代理，使用代理之后可以让爬虫伪装自己的...
+           </div>
+        </div>
+         <div class="tab2">
+           <div class="title">爬虫代理哪家强？十大付费代理详细对比评测出炉！</div>
+           <div class="content">
+             前言 随着大数据时代的到来，爬虫已经成了获取数据的必不可少的方式，做过爬虫的想必都深有体会，爬取的时候莫名其妙 IP 就被网站封掉了，毕竟各大网站也不想自己的数据被轻易地爬走。 对于爬虫来说，为了解决封禁 IP 的问题，一个有效的方式就是使用代理，使用代理之后可以让爬虫伪装自己的...
+           </div>
+        </div>
+         <div class="tab2">
+           <div class="title">爬虫代理哪家强？十大付费代理详细对比评测出炉！</div>
+           <div class="content">
+             前言 随着大数据时代的到来，爬虫已经成了获取数据的必不可少的方式，做过爬虫的想必都深有体会，爬取的时候莫名其妙 IP 就被网站封掉了，毕竟各大网站也不想自己的数据被轻易地爬走。 对于爬虫来说，为了解决封禁 IP 的问题，一个有效的方式就是使用代理，使用代理之后可以让爬虫伪装自己的...
+           </div>
+        </div>
+
+
+      </div>
+      <div class="wrapr">
+        <div class="cs_list2 col">
+          <div class="ivu-carousel-list">
+            <Carousel v-model="value1" loop autoplay arrow="always">
+              <CarouselItem>
+                <div class="demo-carousel">
+                  <img src="https://qiniu.cuiqingcai.com/wp-content/uploads/2016/03/20-234x220.jpg" />
+                  </div>
+              </CarouselItem>
+              <CarouselItem>
+                <div class="demo-carousel">
+                  <img src="https://qiniu.cuiqingcai.com/wp-content/uploads/2015/02/wallpaper_5226849-1.jpg" />
+                  </div>
+              </CarouselItem>
+              <CarouselItem>
+                <div class="demo-carousel">
+                  <img src="https://qiniu.cuiqingcai.com/wp-content/uploads/2016/03/20-234x220.jpg" />
+                  </div>
+              </CarouselItem>
+              <CarouselItem>
+                <div class="demo-carousel">
+                  <img src="https://qiniu.cuiqingcai.com/wp-content/uploads/2015/02/wallpaper_5226849-1.jpg" />
+                  </div>
+              </CarouselItem>
+            </Carousel>
+          </div>
+        </div>
+         <div class="clear"></div>
+         <div class="cs_like">
+           <div class="bg_title">猜你喜欢</div>
+           <ul class="list">
+             <li><a href="">可视化监控你的爬虫快来学习怎么可视化监控你的爬虫</a><span>2018-11-26</span></li>
+             <li><a href="">可视化监控你的爬虫快来学习怎么可视化监控你的爬虫</a><span>2018-11-25</span></li>
+             <li><a href="">可视化监控你的爬虫快来学习怎么可视化监控你的爬虫</a><span>2018-11-24</span></li>
+             <li><a href="">可视化监控你的爬虫快来学习怎么可视化监控你的爬虫</a><span>2018-11-23</span></li>
+             <li><a href="">可视化监控你的爬虫快来学习怎么可视化监控你的爬虫</a><span>2018-11-22</span></li>
+           </ul>
+         </div>
+
+          <div class="cs_like biaoqian">
+           <div class="bg_title">我的标签</div>
+           <ul>
+             <li v-bind:style="bg"><a href="">paython</a></li>
+             <li><a href="">nodejs</a></li>
+             <li><a href="">php</a></li>
+             <li><a href="">java</a></li>
+             <li><a href="">npm</a></li>
+             <li><a href="">css</a></li>
+             <li><a href="">html</a></li>
+             <li><a href="">vue</a></li>
+             <li><a href="">react</a></li>
+             <li><a href="">angular</a></li>
+           </ul>
+         </div>
+
+
+      </div>
     </div>
-    <div class="cs_list1 col">
-      <div class="bigtitle">每日推荐</div>
-      <ul>
-        <li>
-          <span class="label-1">1</span>
-          <a href="">Python2爬虫学习系列教程</a>
-          <p>
-            <span class="muted">
-      			<a href="javascript:;" data-action="ding" data-id="1052" id="Addlike" class="action">
-      			<Icon type="md-heart-outline" />
-      			<span class="count">30620</span> 喜欢
-          </a>
-          </span>
-          </p>
-        </li>
-        <li>
-          <span  class="label-2">2</span>
-          <a href="">Python2爬虫学习系列教程</a>
-          <p>
-            <span class="muted">
-      			<a href="javascript:;" data-action="ding" data-id="1052" id="Addlike" class="action">
-      			<Icon type="md-heart-outline" />
-      			<span class="count">30620</span> 喜欢
-          </a>
-          </span>
-          </p>
-        </li>
-        <li>
-          <span  class="label-3">3</span>
-          <a href="">Python2爬虫学习系列教程</a>
-          <p>
-            <span class="muted">
-      			<a href="javascript:;" data-action="ding" data-id="1052" id="Addlike" class="action">
-      			<Icon type="md-heart-outline" />
-      			<span class="count">30620</span> 喜欢
-          </a>
-          </span>
-          </p>
-        </li>
-        <li>
-          <span  class="label-4">4</span>
-          <a href="">Python2爬虫学习系列教程</a>
-          <p>
-            <span class="muted">
-      			<a href="javascript:;" data-action="ding" data-id="1052" id="Addlike" class="action">
-      			<Icon type="md-heart-outline" />
-      			<span class="count">30620</span> 喜欢
-          </a>
-          </span>
-          </p>
-        </li>
-        <li>
-          <span  class="label-5">5</span>
-          <a href="">Python2爬虫学习系列教程</a>
-          <p>
-            <span class="muted">
-      			<a href="javascript:;" data-action="ding" data-id="1052" id="Addlike" class="action">
-      			<Icon type="md-heart-outline" />
-      			<span class="count">30620</span> 喜欢
-          </a>
-          </span>
-          </p>
-        </li>
-      </ul>
-    </div>
-    <div class="cs_list2 col">
-    	<div class="ivu-carousel-list">
-    		<Carousel v-model="value1" loop>
-		        <CarouselItem>
-		            <div class="demo-carousel">
-		            	<img src="https://qiniu.cuiqingcai.com/wp-content/uploads/2016/03/20-234x220.jpg" />
-		            </div>
-		        </CarouselItem>
-		        <CarouselItem>
-		            <div class="demo-carousel">
-		            		<img src="https://qiniu.cuiqingcai.com/wp-content/uploads/2015/02/wallpaper_5226849-1.jpg" />
-		            </div>
-		        </CarouselItem>
-		        <CarouselItem>
-		            <div class="demo-carousel">
-		            	<img src="https://qiniu.cuiqingcai.com/wp-content/uploads/2016/03/20-234x220.jpg" />
-		            </div>
-		        </CarouselItem>
-		        <CarouselItem>
-		            <div class="demo-carousel">
-		            	<img src="https://qiniu.cuiqingcai.com/wp-content/uploads/2015/02/wallpaper_5226849-1.jpg" />
-		            </div>
-		        </CarouselItem>
-	   		 </Carousel>
-    	</div>
-    </div>
+  
   </div>
 </template>
 <script>
 export default {
   data() {
     return {
-      value1: 0
+      value1: 0,
+      bg:{
+        background:''
+      }
     }
   },
   methods: {
+    changecolor(){
+       return this.bg.background='#'+Math.floor(Math.random()*0xffffff).toString(16);
+    }
 
+  },created(){
+    return this.bg.background='#'+Math.floor(Math.random()*0xffffff).toString(16);
   }
 }
 
 </script>
-<style >
+<style>
+.clear{
+  clear: both;
+}
+.wrapl {
+  width: 70%;
+  float: left;
+  background: #f9f3f3;
+}
+
+.wrapr {
+  width: 30%;
+  float: left;
+  background: #eee;
+}
+
 .col {
   float: left;
 }
 
 .cs_list1 {
-  width: 33%;
+  width: 48%;
   padding: 10px;
+  background: #fff;
+  margin-left: 1%;
 }
 
 .cs_list1 ul li {
@@ -235,11 +329,11 @@ export default {
 }
 
 .cs_list2 {
-  width: 30%;
+  width: 100%;
   height: 100%;
   background: #eee;
   float: right;
-    padding-top: 34px;
+  padding-top: 10px;
 }
 
 .bigtitle {
@@ -253,15 +347,87 @@ export default {
   margin-bottom: 10px;
 }
 
-.ivu-carousel-list, .ivu-carousel-track {
-    position: relative;
-    display: block;
-    overflow: hidden;
-    height: 190px;
+.ivu-carousel-list,
+.ivu-carousel-track {
+  position: relative;
+  display: block;
+  overflow: hidden;
+  height: 190px;
 }
-.demo-carousel img{
-	display: block;
-	width: 100%;
-	height: auto;
+
+.demo-carousel img {
+  display: block;
+  width: 100%;
+  height: auto;
 }
+
+
+.tab2{
+  width: 97%;
+  margin-top: 10px;
+  clear: both;
+  background: #fff;
+  margin-left: 8px;
+  padding: 10px;
+  margin-bottom: 10px;
+}
+
+.title{
+ text-align: left;
+ font-size: 20px;
+ color: #666;
+ padding: 5px 0;
+}
+.content{
+  text-align: left;
+  font-size: 14px;
+  color: #333;
+}
+
+.cs_like{
+  background: #fff;
+  margin-top: 10px;
+   padding: 10px;
+}
+.cs_like .bg_title{
+    color: #666;
+    font-size: 18px;
+    padding: 5px 0;
+    border-bottom: 1px solid #ccc;
+    text-align: left;
+}
+
+.cs_like .list li{
+  text-align: left;
+  padding: 6px 0;
+  border-bottom: 1px dashed #ccc;
+}
+
+.cs_like .list li a{
+  display: inline-block;
+  width: 70%;
+  overflow: hidden;
+  white-space: nowrap;
+  text-overflow: ellipsis;
+}
+.cs_like .list li span{
+  float: right;
+}
+.biaoqian{
+      overflow: hidden;
+    margin-bottom: 10px;
+}
+.biaoqian ul{
+  margin-top: 10px;
+}
+.biaoqian ul li{
+  float: left;
+  padding: 5px;
+  border-radius: 5px;
+  background: #ff4400;
+  margin-right: 5px;
+  margin-bottom: 10px;
+}
+
+
 </style>
